8e8e9624c1a6a52e589302d480de2078da728558
HinakoNozaki <s1711462@s.tsukuba.ac.jp>
2019-10-01 14:52:19 +0900

    Initial Commit

---
+++ Warikan2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Warikan2
+//
+//  Created by 野崎陽奈子 on 2019/10/01.
+//  Copyright © 2019 野崎陽奈子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
